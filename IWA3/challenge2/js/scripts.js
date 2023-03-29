42d472522f16917bffb5b744fd104e59b7060965
jarreed <jarreedw@gmail.com>
2023-03-29 21:22:52 +0200

    matched console logging to intstruction

--- IWA3/challenge2/js/scripts.js
+++ IWA3/challenge2/js/scripts.js
@@ -2,4 +2,4 @@ import { role as nwabisaRole } from "./nwabisa.js";
 import { role as johannesRole } from "./johannes.js";
 import { role as alexRole } from "./alex.js";
 
-console.log('Roles:', nwabisaRole, johannesRole, alexRole);
+console.log('Roles: ' + nwabisaRole +", "+ johannesRole +", "+ alexRole);
