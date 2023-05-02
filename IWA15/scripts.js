56cf097e1445ff237f7bc9bd2ba324063e1cd2b5
jarreed <jarreedw@gmail.com>
2023-05-02 09:53:50 +0200

    solved challenge of iwa15

--- IWA15/scripts.js
+++ IWA15/scripts.js
@@ -8,22 +8,20 @@ const data = {
 
 // Only edit below
 
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
+const first = data.lists[0][1] //[15, 11, 13, 7, 5]
+const second = data.lists[1][1] //[2, 6, 8, 4, 14, 12, 10]
+const third = data.lists[2][1] //[9, 3, 1]
 
 const result = []
 
 const extractBiggest = () => {
-	if (first[-1] > second[-1]) {
-		return first
-	}
-
-	if (third[-1] < 1) {
-		return second
-	}
-	
-	return third
+	if (first[first.length-1] > second[second.length-1] && first[first.length-1] > third[third.length-1]){
+        return first.pop()
+	} else if (second[second.length-1] > third[third.length-1]) {
+        return second.pop()
+	} else {
+        return third.pop()
+    }
 }
 
 // Only edit above
